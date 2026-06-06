25aec9802085efa31cac5a8edc374318473f38fd
vansh <vanshduggal37@gmail.com>
2026-06-06 13:42:59 +0530

    product building

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from api.signup import router as signup_router
 from api.login import router as login_router
+from api.orders import router as orders_router
 app=FastAPI()
 
 app.include_router(signup_router)
 app.include_router(login_router)
+app.include_router(orders_router)
 @app.get("/")
 def read_root():
     return {"Message":"Finnaly I am able to run my first API"}
