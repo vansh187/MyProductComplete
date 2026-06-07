a57deae183ac135e9eaaae9d9e5d6107c9d1659b
vansh <vanshduggal37@gmail.com>
2026-06-07 21:44:18 +0530

    verify login token

--- utils/jwt_handler.py
+++ utils/jwt_handler.py
@@ -20,3 +20,26 @@ def verify_token(token: str):
         return payload  # contains user info (email, exp)
     except JWTError:
         return None
+def decode_token(token: str):
+    SECRET_KEY = "mysecretkey123"
+    ALGORITHM = "HS256" 
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload  # contains user info (email, exp)
+    except JWTError:
+        raise Exception("JWT decoding error"+str(JWTError))
+    except jwt.ExpiredSignatureError:
+            raise Exception("Token expired"+str(jwt.ExpiredSignatureError))
+    except jwt.InvalidTokenError:
+            raise Exception("Invalid token"+str(jwt.InvalidTokenError))    
+
+def get_user_from_token(request):
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header:
+        raise Exception("Authorization header missing")
+
+    token = auth_header.replace("Bearer ", "")
+    payload = decode_token(token)
+
+    return payload
