333c1f7c788915dd154e2237a4273832506e07ce
vansh <vanshduggal37@gmail.com>
2026-06-09 23:39:10 +0530

    trade Order api

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from pydantic import BaseModel
 from api.signup import router as signup_router
 from api.login import router as login_router
 from api.orders import router as orders_router
+from api.trade import router as trade_history
 app=FastAPI()
 
 app.include_router(orders_router)
 app.include_router(signup_router)
 app.include_router(login_router)
+app.include_router(trade_history)
+
 @app.get("/")
 def read_root():
     return {"Message":"Finnaly I am able to run my first API"}
