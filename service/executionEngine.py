4990aa3dde66424e86d109ff8f2e2f6fb659009c
Vansh Duggal <40709014+vansh187@users.noreply.github.com>
2026-06-14 01:51:55 +0530

    import commit

--- service/executionEngine.py
+++ service/executionEngine.py
@@ -5,7 +5,7 @@ from service.tradeHistoryService import TradeHistoryService as tradeService
 from dotenv import load_dotenv
 from database.ConnectionFactory import ConnectionFactory
 import os
-from service.matchingEngine.matchingEngineService.matching_engine_service import MatchingEngineService as MatchingEngine
+from service.matchingEngine.matchingEngineService import MatchingEngineService as MatchingEngine
 
 
 load_dotenv()
