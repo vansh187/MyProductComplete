f10bcaff76813f9856c052c31fa77ba959131baa
vansh <vanshduggal37@gmail.com>
2026-06-15 02:18:40 +0530

    dashboard again

--- productdto/DashboardDTO.py
+++ productdto/DashboardDTO.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 from productdto import OrdersSummaryDTO 
-from productdto.portfolioDTO import PortfolioDTO 
+from productdto.PortfolioSummaryDTO import PortfolioSummaryDTO 
 from productdto.TradesSummaryDTO import  TradesSummaryDTO
 
 @dataclass
@@ -9,6 +9,6 @@ class DashboardDTO:
     user_id: int
     orders: OrdersSummaryDTO
     trades: Optional[TradesSummaryDTO] = None
-    portfolio: Optional[PortfolioDTO] = None
+    portfolio: Optional[PortfolioSummaryDTO] = None
     last_updated: Optional[str] = None
     
