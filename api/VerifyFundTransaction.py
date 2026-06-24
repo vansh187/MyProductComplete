f7a8e60e15d7b06432ab886ca231d1c673e0e4ae
vansh <vanshduggal37@gmail.com>
2026-06-24 17:41:15 +0530

    loggers added

--- api/VerifyFundTransaction.py
+++ api/VerifyFundTransaction.py
@@ -56,11 +56,11 @@ def process_webhook_data_in_background(event_data: dict):
             if event_type in ("payment.captured", "payment.authorized"):
                 payment_entity = event_data["payload"]["payment"]["entity"]
                 print("paymentEntity  "+payment_entity)
-                notes = payment_entity.get("notes") or {}
-                user_id = notes.get("user_id")
-                print("userId from notes"+user_id)
-                user_id=payment_entity.get("user_id")
-                print("userId from payment_entity"+user_id)
+                #notes = payment_entity.get("notes") or {}
+                currentUser=get_current_user()
+                print(currentUser)
+                user_id=currentUser["user_id"]
+                print(user_id)
                 if not user_id:
                     print("Webhook: user_id missing from payment notes, skipping wallet update")
                     return
